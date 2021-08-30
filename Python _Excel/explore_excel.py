3fab510ce064604f9b3775ad42859b8b9780f868
kkdart <52509568+kkdart@users.noreply.github.com>
2021-08-29 17:51:21 -0700

    Updated

--- Python _Excel/explore_excel.py
+++ Python _Excel/explore_excel.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import pandas as pd
+from openpyxl.workbook import Workbook
+
+df_excel = pd.read_excel('regions.xlsx')
+df_csv = pd.read_csv('Names.csv')
+df_txt = pd.read_csv('data.txt')
+
+print(df_excel)
