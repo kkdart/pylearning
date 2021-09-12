8bb5a6653dc71ae6dedad4d3757cf7ce472b4654
kkdart <52509568+kkdart@users.noreply.github.com>
2021-09-11 22:13:02 -0700

    Update viewing.py

--- Python _Excel/viewing.py
+++ Python _Excel/viewing.py
@@ -4,4 +4,9 @@ from openpyxl import Workbook
 df = pd.read_csv('Names.csv', header=None)
 df.columns = ['First', 'Last', 'Address', 'City','State','Area Code','Salary']
 
-print(df.columns)
+#print(df[['State','Area Code']])
+
+#Print Slice of first 3 rows (indexed 0 to 2)
+#print(df['First'][0:3])
+
+print(df.iloc[1])
