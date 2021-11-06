3eadab4b834de0b05d0cd643bad7539fbfa75b49
kkdart <52509568+kkdart@users.noreply.github.com>
2021-11-06 09:47:03 -0700

    Added pointers.py

---
+++ Data Structures & Algorithms/pointers.py
@@ -0,0 +1,30 @@
+num1 = 11
+
+num2 = num1
+
+print("Before the value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+num1 = 22
+
+print("\nAfter value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+dict1 = {'value': 11}
+dict2 = dict1
+
+print("\nBefore value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+dict1['value']=22
+
+print("\nAfter value is updated:")
+print('dict1 =', dict1)
+print('dict2 =', dict2)
+
+dict3 = {'value':57}
+dict2 = dict3
+dict1 = dict3
