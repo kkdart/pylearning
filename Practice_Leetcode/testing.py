b90927742fbb890712d64ebb65d78dd252c6cd06
kkdart <52509568+kkdart@users.noreply.github.com>
2021-12-29 19:20:39 -0800

    Update testing.py

--- Practice_Leetcode/testing.py
+++ Practice_Leetcode/testing.py
@@ -1,3 +1,4 @@
+'''
 def rotLeft(a, d):
     d = d%len(a)
     a = a[d:]+a[:d]
@@ -6,3 +7,12 @@ def rotLeft(a, d):
 a = [1, 2, 3, 4, 5]
 
 print(rotLeft(a,6))
+'''
+
+#String Slicing
+num_strings = 2
+s1 = "Hacker"
+s2 = "Rank"
+
+for i in range(num_strings):
+    print(s1)
