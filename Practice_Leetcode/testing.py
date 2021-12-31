de241b51d711539a43d1e46bec34b024c597672b
kkdart <52509568+kkdart@users.noreply.github.com>
2021-12-31 09:05:47 -0800

    Update testing.py

--- Practice_Leetcode/testing.py
+++ Practice_Leetcode/testing.py
@@ -7,7 +7,7 @@ def rotLeft(a, d):
 a = [1, 2, 3, 4, 5]
 
 print(rotLeft(a,6))
-'''
+
 
 #String Slicing
 num_strings = 2
@@ -16,3 +16,33 @@ s2 = "Rank"
 
 for i in range(num_strings):
     print(s1)
+
+
+arr = [1, 4, 3, 2]
+arr = ["1", "2", "3"]
+
+print(" ".join(arr))
+'''
+#input 
+'''
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+'''
+d = {}
+c = input()
+for i in range(c):
+    l = input().split(" ",2)
+    print(l)
+    d.update({ l[0]:l[1]})
+
+for i in range(c):
+    t = input()
+    if t in d:
+        print(t+"="+d.get(t))
+    else:
+        print("Not Found")
