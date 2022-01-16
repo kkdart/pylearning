176da3df4e920aa73df09de9f658bb6a32e127a6
kkdart <52509568+kkdart@users.noreply.github.com>
2022-01-16 14:16:39 -0600

    Predictive Analytics

---
+++ Predictive Analytics/01_03/01_03_Begin.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 15:46:31 2019
+
+@author: berkunis
+"""
+##############################################01_02_PythonLibraries#####################################################
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt 
+
+#import data
+data = pd.read_csv("../Datasets/insurance.csv")
+
+#see the first 15 lines of data
+print(data.head(15))
+
+############################################01_03_HandlingMissingValues###################################################
+
+#check how many values are missing (NaN) before we apply the methods below 
+count_nan = data.isnull().sum()
+print(count_nan[count_nan>0])
+
+
+
+#fill in the missing values (there are so many other methods out there)
+data['bmi'].fillna(data['bmi'].mean(), inplace = True)
+
+count_nan = data.isnull.sum()
+print(count_nan[count_nan>0])
+#check how many values are missing (NaN) - after we filled in the NaN
+
+
+############################################Vizualization################################################################
