9517ef9170fa9c5ce0540e3aea216b871284df3d
kkdart <52509568+kkdart@users.noreply.github.com>
2022-06-16 22:31:07 -0700

    u

--- Python_Database/sqlalchemy-workspace/create_users_table.py
+++ Python_Database/sqlalchemy-workspace/create_users_table.py
@@ -4,13 +4,27 @@ connection = sqlite3.connect('challenge.db')
 
 cursor = connection.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS Users(User_Id TEXT, First_Name TEXT, Last_Name TEXT, Email TEXT)''')
+# cursor.execute('''CREATE TABLE IF NOT EXISTS Users(User_Id TEXT, First_Name TEXT, Last_Name TEXT, Email TEXT)''')
 
-user_list = [('1', "John", "Smith", "jsmith@gmail.com"),
-('2', "Johnny", "Appleseed", "jseed@gmail.com"),
-('3', "Steph", "Curry", "scurry@gmail.com"),
-('4', "Steven", "Smith", "ssmith@gmail.com"),
-('5', "Bob", "Oregon", "boregon@gmail.com")]
+# user_list = [('1', "John", "Smith", "jsmith@gmail.com"),
+# ('2', "Johnny", "Appleseed", "jseed@gmail.com"),
+# ('3', "Steph", "Curry", "scurry@gmail.com"),
+# ('4', "Steven", "Smith", "ssmith@gmail.com"),
+# ('5', "Bob", "Oregon", "boregon@gmail.com")]
 
 
-cursor.execute("INSERT INTO Users VALUES(")
+# cursor.executemany('INSERT INTO Users VALUES(?,?,?,?)',user_list)
+# connection.commit()
+
+
+# records = cursor.execute("SELECT * from Users")
+
+# for i in records:
+#     print(i)
+
+records = cursor.execute("SELECT Email from Users")
+
+for i in records:
+    print(i)
+
+connection.close()
