36698fed32bdc7a2b905006f08aa74b5967c38d0
kkdart <52509568+kkdart@users.noreply.github.com>
2022-07-15 13:57:40 -0700

    Update postgres query

--- Python_Database/postgres-db.py
+++ Python_Database/postgres-db.py
@@ -1,24 +1,36 @@
 #pip3 install psycopg2
+# pip3 install psycopg2-binary
+
 import psycopg2
+import csv
 
 conn = psycopg2.connect(database ="red30",
     user="postgres",
     password="pwtesting",
-    host="localhost",
+    host="10.1.1.40",
     port="5432")
 
-cursor = conn.cursor()
+cur = conn.cursor()
 
-cursor.execute('''CREATE TABLE Sales
+#Query to create sales table
+create_red30 = '''CREATE TABLE Sales
     (ORDER_NUM INT PRIMARY KEY,
         ORDER_TYPE TEXT,
         CUST_NAME TEXT,
         PROD_NUMBER TEXT,
         PROD_NAME TEXT,
-        QUNATITY INT,
+        QUANTITY INT,
         PRICE REAL,
         DISCOUNT REAL,
-        ORDER_TOTAL REAL);''')
+        ORDER_TOTAL REAL);'''
+
+cur.execute(create_red30)
+
+#Insert data from a csv into the sales table
+with open('/Users/kyryl/Desktop/red30-postgres.csv','r') as f:
+    # Notice that we don't need the `csv` module.
+    next(f) # Skip the header row.
+    cur.copy_from(f, 'sales', sep=',')
 
 conn.commit()
 conn.close()
