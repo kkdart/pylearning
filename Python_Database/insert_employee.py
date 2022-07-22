fc8a069748f249a8a536e5ce5f26b6e3b7987d78
kkdart <52509568+kkdart@users.noreply.github.com>
2022-07-21 20:59:26 -0700

    Update

---
+++ Python_Database/insert_employee.py
@@ -0,0 +1,30 @@
+#win pip3 install psycopg2
+#mac os pip3 install psycopg2-binary
+
+import psycopg2
+
+conn = psycopg2.connect(database ="red30",
+    user="postgres",
+    password="pwtesting",
+    host="10.1.1.40",
+    port="5432")
+
+cur = conn.cursor()
+
+#Query to create sales table
+create_employee = '''CREATE TABLE IF NOT EXISTS employee(
+    ID SERIAL PRIMARY KEY,
+    NAME TEXT NOT NULL,
+    AGE INT NOT NULL,
+    SALARY TEXT);'''
+
+cur.execute(create_employee)
+
+#Insert data from a csv into the sales table
+with open('/Users/kyryl/Desktop/load.csv','r') as f:
+    # Notice that we don't need the `csv` module.
+    next(f) # Skip the header row.
+    cur.copy_from(f, 'employee', sep=',', columns = ('name', 'age', 'salary'))
+
+conn.commit()
+conn.close()
